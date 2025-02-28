354119f4a4ef7be60a63702a3a2cf186fb8f1cb6
Zephyrrr <kuangdadu@gmail.com>
2025-02-28 04:39:59 +0000

    feat: add tags to Post model and create seed data

---
+++ prisma/seed.ts
@@ -0,0 +1,78 @@
+import { PrismaClient, Prisma } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    email: "1@1.com",
+    password: "test1234",
+  },
+];
+
+const postData: Prisma.PostCreateInput[] = [
+  {
+    title: "My first post",
+    tags: ["test", "test2"],
+    content: `# Header 1
+
+## Header 2
+
+### Header 3
+
+#### Header 4
+
+### 2. Emphasis
+
+You can emphasize text using _italic_ or **bold** formatting.
+
+- _Italic text_
+- **Bold text**
+- **_Bold and italic text_**
+
+### 3. Lists
+
+#### Unordered List
+
+- Item 1
+- Item 2
+  - Subitem 2.1
+  - Subitem 2.2
+
+#### Ordered List
+
+1. First item
+2. Second item
+   1. Subitem 2.1
+   2. Subitem 2.2
+
+### 4. Links and Images
+
+You can add links and images:
+
+- [OpenAI](https://www.openai.com)
+- ![Sample Image](https://picsum.photos/200)
+
+### 5. Code
+
+You can include inline code like \`print("Hello, World!")\` or code blocks:
+
+\`\`\`python
+def hello_world():
+    print("Hello, World!")
+\`\`\`
+
+`,
+  },
+];
+
+export async function main() {
+  for (const u of userData) {
+    await prisma.user.create({ data: u });
+  }
+
+  for (const p of postData) {
+    await prisma.post.create({ data: p });
+  }
+}
+
+main();
