3c76ec271111404bb566a25996450ac8e9d98a6f
Zephyrrr <kuangdadu@gmail.com>
2025-03-04 07:39:56 +0000

    refactor: add full width to login and signup page containers

--- app/(admin)/login/page.tsx
+++ app/(admin)/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/auth/LoginForm";
 
 export default async function LoginPage() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen w-full">
       <LoginForm />
     </div>
   );
--- app/(admin)/signup/page.tsx
+++ app/(admin)/signup/page.tsx
@@ -2,7 +2,7 @@ import { SignupForm } from "@/components/auth/SignupForm";
 
 export default function SignupPage() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen w-full">
       <SignupForm />
     </div>
   );
